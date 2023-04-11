baf01f407c5b9e2150489bdc243ef5bc7aae5682
dunkuenfai <56312707+dunkuenfai@users.noreply.github.com>
2023-04-11 21:01:01 +0800

    Add files via upload

---
+++ longhu.py
@@ -0,0 +1,196 @@
+
+import requests
+import time
+import json
+import datetime
+from chinese_calendar import is_workday, is_holiday
+from datetime import date,datetime
+from datetime import timedelta
+headers = {
+    "accept": "*/*",
+    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+    "sec-ch-ua": "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Microsoft Edge\";v=\"110\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "Referer": "https://emdata.eastmoney.com/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  }
+msg=''
+ 
+
+
+def is_work_day():
+    """判断是否工作日"""
+    now = datetime.now()
+    # today = now.strftime("%Y%m%d") #获取到"20210111"这个字符串
+    # nowatime = now.strftime("%H:%M:%S") #获取到"20:05:51"这个字符串
+    # today = now.strftime("%Y,%m,%d") #获取到"2021-01-11"这个字符串
+    april_last = date(2023, 3,19)   # datetime.date
+    # april_last = datetime.now().strftime("%Y%m%d")
+    print(is_workday(now))     # True
+    print(is_holiday(april_last))     # False
+    print(april_last.weekday())       # 5-星期六
+    # print(str(today))
+    lastday=get_pervious_work_day(now)
+    print(lastday)
+ 
+ 
+def get_pervious_work_day(day: datetime):
+    """获取上一个工作日"""
+    day = day - timedelta(days=1)
+    if is_workday(day):
+        return day
+    return get_pervious_work_day(day)
+
+def longhu():
+    print('获取龙虎榜。。。。。。。。。。')
+    global msg
+    try:
+        now = datetime.now()
+        today = now.strftime("%Y-%m-%d")
+        lastday=get_pervious_work_day(now).strftime("%Y-%m-%d")
+        url = "https://datacenter.eastmoney.com/securities/api/data/get?type=RPT_ORGANIZATION_TRADE_DETAILSNEW&sty=ALL&source=DataCenter&client=WAP&p=1&ps=20&sr=-1&st=RATIO&filter=(TRADE_DATE%3E=%27"+lastday+"%27)(TRADE_DATE%3C=%27"+today+"%27)"
+
+
+        # 发送一个请求
+
+        response = requests.get(url,headers)
+
+        response.raise_for_status()
+        success=response.json()['success']
+        if(success==False):
+            print('返回数据为空')
+            return
+        # print(response.text)
+        data = response.json()['result']['data']
+        msg=msg+('-------------------------------------------\n')
+        msg=msg+('          龙虎榜机构买入超5%：\n')
+        msg=msg+('-------------------------------------------\n')
+        msg=msg+('名   称 '+'代   码 '+'当天升幅 '+'买入占比\n')
+        
+        for i in data:
+            if(i['RATIO']>5):
+                msg=msg+(i['SECURITY_NAME_ABBR']+" "+i["SECURITY_CODE"]+" "+str(round(i['CHANGE_RATE'],2))+'%'+" "+str(round(i['RATIO'],2))+'%\n')
+        msg=msg+('-------------------------------------------'+'\n')
+        
+    except Exception as e:
+
+         msg=msg+("读取龙虎榜失败"+e)
+
+def get_organization():
+    '''获取机构3天上涨排行'''
+    print('获取机构3天上涨排行。。。。。。。。。。')
+    global msg
+    msg=msg+('-------------------------------------------\n')
+    msg=msg+('机构买入3天上涨排行近期交易数据\n')
+    msg=msg+('-------------------------------------------\n')
+    # msg.join ('机构买入3天上涨排行近期交易数据')
+    try:
+        now = datetime.now()
+        today = now.strftime("%Y-%m-%d")
+        lastday=get_pervious_work_day(now).strftime("%Y-%m-%d")
+        url = "https://datacenter.eastmoney.com/securities/api/data/get?type=RPT_OPERATEDEPT_RETURN_RANKING&sty=ALL&source=DataCenter&client=WAP&p=1&ps=300&sr=-1&st=AVERAGE_INCREASE_2DAY&filter=(STATISTICSCYCLE=%2203%22)&?v=02695676277618384"
+        
+        # 发送一个请求
+
+        response = requests.get(url,headers)
+
+        response.raise_for_status()
+        success=response.json()['success']
+        # print(response.json())
+        if(success==False):
+            msg=msg+('返回数据为空\n')
+            return
+        # print(response.text)
+        data = response.json()['result']['data']
+        # print(data[0]['TOTAL_BUYER_SALESTIMES_1DAY'])
+        # return
+        # get_org_buy(data[0]['OPERATEDEPT_CODE'])
+        # return
+        for item in data:
+            # print(item['TOTAL_BUYER_SALESTIMES_1DAY'])
+            if(type(item['TOTAL_BUYER_SALESTIMES_1DAY'])==int and item['TOTAL_BUYER_SALESTIMES_1DAY']>5):
+                    
+                if(get_org_buy(item['OPERATEDEPT_CODE'])):
+                    
+                    msg=msg+('      '+item['OPERATEDEPT_CODE']+" "+
+                    str(item['TOTAL_BUYER_SALESTIMES_1DAY'])+" "+
+                    str(round(item['AVERAGE_INCREASE_3DAY'],2))+'%'+" "+
+                    item['OPERATEDEPT_NAME']+"\n"
+                    )
+                    msg=msg+('==================================================\n')
+
+
+
+        
+
+    except Exception as e:
+
+        print("读取龙虎榜失败", e)    
+
+
+def get_org_buy(code):
+    '''获取机构近5天交易'''
+    global msg
+    url='https://datacenter.eastmoney.com/securities/api/data/get?type=RPT_OPERATEDEPT_TRADE_DETAILSNEW&sty=SECURITY_CODE,SECURITY_NAME_ABBR,TRADE_DATE,OPERATEDEPT_NAME,ACT_BUY,ACT_SELL,NET_AMT,EXPLANATION,D1_CLOSE_ADJCHRATE,D2_CLOSE_ADJCHRATE,D3_CLOSE_ADJCHRATE,D5_CLOSE_ADJCHRATE,D10_CLOSE_ADJCHRATE,D20_CLOSE_ADJCHRATE,D30_CLOSE_ADJCHRATE,SECUCODE&p=1&ps=20&st=TRADE_DATE&sr=-1&filter=(OPERATEDEPT_CODE%3D%22'+code+'%22)&?v=018168881062146247'
+    response = requests.get(url,headers)
+    response.raise_for_status()
+    success=response.json()['success']
+    # print(response.json())
+    if(success==False):
+        msg=msg+('返回数据为空\n')
+        return
+    data = response.json()['result']['data']
+    for item in data:
+        # print(item['TRADE_DATE'])
+        time1 = time.mktime(time.strptime(item['TRADE_DATE'], '%Y-%m-%d %H:%M:%S'))
+        nowTime =  time.mktime(time.strptime(datetime.now().strftime('%Y-%m-%d'), '%Y-%m-%d'))
+        # print(item['NET_AMT']>0)
+        if((nowTime - time1)<5*24*60*60 and item['NET_AMT']>0):
+            msg=msg+(item['SECURITY_CODE']+" "+item['SECURITY_NAME_ABBR']+" "+item['TRADE_DATE']+" "+str(item['NET_AMT'])+"\n")
+            return True
+    return False
+    # print(data)
+
+"""pushplus"""
+def pushMsn(token,title,content):
+
+    try:
+
+        url = "http://www.pushplus.plus/send"
+        params={
+            "token":{token},
+            "title":title,
+            "content":content,
+            "data":[]
+            # "template":"json"
+        }
+
+        response = requests.post(url, params =params)
+
+        response.raise_for_status()
+
+        response.encoding = response.apparent_encoding
+
+        data=json.loads(response.text)
+
+        print(data)
+        
+    except Exception as e:
+
+        print("push消息错误", e)
+
+def main():
+    global msg
+    msg=msg+'###########################################\n'
+    get_organization()
+    msg=msg+'###########################################\n'
+    longhu()
+    pushMsn('9265ac3f9ab34138a56f68a1c4624e93','龙虎榜',msg)
+    print(msg)
+if __name__ == "__main__":
+
+    main()
---
+++ sunion.py
@@ -0,0 +1,241 @@
+
+import requests
+import time
+import json
+import os 
+from datetime import date,datetime
+from datetime import timedelta
+#https://weapp.zhdy.es668.cn/records?access-token=hJMVgtfhfmBJrlrzV-efVM2HiGonH5OA
+
+token=os.environ['znl']
+# 签到
+msg=""
+params = {"records":[
+            {"value":"按时起床,一日之际在于晨","type":"10"},
+            {"value":"做好早餐给小孩","type":"10"},
+            {"value":"早上送女儿上学","type":"10"},
+            {"value":"做好防疫措施","type":"10"},
+            {"value":"上班路上礼让行人","type":"10"},
+            {"value":"路上顺利顺路接同事","type":"10"},
+            {"value":"坚持8点前到岗","type":"10"},
+            {"value":"到公司见到同事打招呼","type":"10"},
+            {"value":"打开办公室窗户通风","type":"10"},
+            {"value":"为办公室煮开水","type":"10"},
+            {"value":"交接班聆听安全之歌，弘扬正能量事","type":"10"},
+            {"value":"交接班合理安排当天工作，提醒同事注意作业安全","type":"10"},
+            {"value":"交完班帮忙收拾凳子","type":"10"},
+            {"value":"按时对装置进行巡检","type":"10"},
+            {"value":"办公尽量使用电子版，减少纸张，节约一分钱","type":"10"},
+            {"value":"保持台面清洁","type":"10"},
+            {"value":"中午午休期间尽量不发出声响以免影响同事休息","type":"10"},
+            {"value":"节约水资源，不浪费一滴水","type":"10"},
+            {"value":"不浪费一度电，及时关闭没有在用电器","type":"10"},
+            {"value":"宣传众和正能量故事","type":"10"},
+            {"value":"感恩众和","type":"10"},
+            {"value":"坚持每天学习","type":"10"},
+            {"value":"坚持写正能量","type":"10"},
+            {"value":"坚持写工作日志","type":"10"},
+            {"value":"下班路上礼让行人","type":"10"},
+            {"value":"辅导儿子做作业","type":"10"},
+            {"value":"检查女儿英语作业情况","type":"10"},
+            {"value":"吃完饭收拾碗筷并洗碗","type":"10"},
+            {"value":"陪父母聊天","type":"10"},
+            {"value":"晚上在家坚持锻炼身体","type":"10"},
+            {"value":"鼓励小孩坚持锻炼身体","type":"10"},
+            {"value":"调好水温给小孩洗澡","type":"10"},
+            {"value":"将换下来的脏衣服拿到洗衣机洗","type":"10"},
+            {"value":"提醒小孩到点睡觉，早睡早起","type":"10"},
+            {"value":"听新闻热点，快速了解国内外大事","type":"10"},
+            {"value":"关注天气预报，了解天气情况","type":"10"},
+            {"value":"做明天的工作计划","type":"10"},
+            {"value":"微笑面对所有困难，给自己打气","type":"10"},
+            ]}
+# params={"records":[{"value":"做好备件计划","type":"10"}]}
+
+def zhengnengliang():
+
+    try:
+
+        url = "https://weapp.zhdy.es668.cn/records?access-token="+token
+        headers = {
+    "host":"weapp.zhdy.es668.cn",
+    "Connection":"keep-alive",
+    "content-type":"application/json",
+    "Accept":"application/json",
+    "Accept-Encoding":"gzip,compress,br,deflate",
+    "User-Agent":"Mozilla/5.0 (iPad; CPU OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.29(0x18001d2f) NetType/WIFI Language/zh_CN",
+    "Referer":"https://servicewechat.com/wx22189b1374bef585/17/page-frame.html"
+
+     }
+
+        # print(params)
+        
+        # 发送一个请求
+
+        response = requests.post(url, json=params, headers=headers)
+
+        response.raise_for_status()
+
+        response.encoding = response.apparent_encoding
+
+        # 由于返回的是JSONP数据，所以不能用response.json()来解析
+
+        content = response.text
+        #print(content)
+        data=json.loads(content)
+        print(data,'\n共发布 ',len(params),' 条正能量' )
+
+        # 截取JSONP中的JSON数据
+        return
+
+        data = json.loads(content[content.find("{"):content.rfind(")")])
+
+        if data.get("error_code") == 0:
+
+            print("发布成功：", data.get("data").get("checkin_num"))
+
+        else:
+
+            print("发布失败，原因：", data.get("error_msg"))
+
+    except Exception as e:
+
+        print("发布错误；", e)
+
+
+def paiming():
+    now=datetime.now()
+    today = now.strftime("%Y-%m-%d")
+    month =now.strftime("%Y-%m-")+"1"
+    # print(month)
+    global  msg
+    try:
+       url = "https://weapp.zhdy.es668.cn/paimings/latest/1?access-token=" +token
+       headers={
+        "host":"weapp.zhdy.es668.cn",
+        "Connection":"keep-alive",
+        "content-type":"application/json",
+        "Accept":"application/json",
+        "Accept-Encoding":"gzip,compress,br,deflate",
+        "User-Agent":"Mozilla/5.0 (iPad; CPU OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.29(0x18001d2f) NetType/WIFI Language/zh_CN",
+        "Referer":"https://servicewechat.com/wx22189b1374bef585/17/page-frame.html"
+       }
+       data={
+           "today":0,
+           "date":[month,today],
+           "sort":"total_record",
+           "keyword":"苏杰万"
+       }
+       response = requests.post(url, json=data, headers=headers)
+       response.raise_for_status()
+       response.encoding = response.apparent_encoding
+       content = response.text
+       data=json.loads(content)["data"]["set"][0]
+       paihang=data["ranking"]
+       children=data["children"]
+       msg=msg+("小组排名"+str(paihang)+"名\n")
+       for item in children:
+           msg=msg+(item["name"]+" "+str(item["total"])+"条\n")
+       msg=msg+"\n"
+
+    except Exception as e:
+        print("查询错误；", e)
+
+
+
+def toutiao():
+
+    try:
+
+        url = "https://www.toutiao.com/api/pc/feed/"
+
+        headers = {
+    "Connection":"keep-alive",
+    "content-type":"application/json",
+    "accept-encoding":"gzip, deflate, br",
+    "Accept-Encoding":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36 Edg/110.0.1587.63"
+
+      }
+
+        
+
+        # 发送一个请求
+
+        response = requests.get(url,headers=headers)
+
+        response.raise_for_status()
+        data = response.json()['data']
+
+        if len(data)<5:
+            print('获取列表小于5条，结束！！！')
+            return
+        for i in range(5):
+            content={}
+            value=data[i]['title']
+            content['value']="阅读新闻《"+value+"》"
+            content['type']=10
+            params['records'].append(content)
+        #print(list)
+        #params['records'].append(list[0])
+
+        
+    except Exception as e:
+
+        print("读取头条失败", e)
+
+
+
+
+"""pushplus"""
+def pushMsn(token,title,content):
+
+    try:
+
+        url = "http://www.pushplus.plus/send"
+        params={
+            "token":{token},
+            "title":title,
+            "content":content,
+            "template":"json"
+        }
+
+        response = requests.post(url, params =params)
+
+        response.raise_for_status()
+
+        response.encoding = response.apparent_encoding
+
+        data=json.loads(response.text)
+
+        #print(data)
+        
+    except Exception as e:
+
+        print("push消息错误", e)
+
+
+
+
+def main():
+    global msg
+    #print("-------------------获取头条列表-------------------")
+
+    toutiao()
+    #print("-------------------获取头条结算-------------------")
+
+    #print("-------------------发布开始-------------------")
+
+    zhengnengliang()
+    # pushMsn('9265ac3f9ab34138a56f68a1c4624e93','龙虎榜','111')
+    paiming()
+
+    #print("-------------------发布结束-------------------")
+    msg=msg+str(params)
+    pushMsn('9265ac3f9ab34138a56f68a1c4624e93','正能量',msg)
+    print(msg)
+
+
+if __name__ == "__main__":
+
+    main()
