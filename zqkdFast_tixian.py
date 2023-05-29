8700db269fa80a8a2ee957f80e109251add51e18
dunkuenfai <56312707+dunkuenfai@users.noreply.github.com>
2023-05-29 15:37:15 +0800

    Add files via upload

--- zqkdFast_tixian.py
+++ zqkdFast_tixian.py
@@ -18,8 +18,14 @@ import os
 import re
 
 user=os.environ['zqkdFastCookie']
+from sendNotify import send
+
+# const notify = $.isNode() ? require('./sendNotify') : '';
+allMessage = ''
+
 userInfos=user.split('@')
 def getScore(userInfo):
+    global allMessage
     userID=re.findall('(?<=uid=).+?(?=&)',userInfo)
     BASE_URL='https://user.youth.cn/v1/user/userinfo.json'
     headers = {
@@ -55,10 +61,12 @@ def getScore(userInfo):
             money=0.3
         else:
             print('青豆少于3000，终止提现！')
+            allMessage =allMessage  +'用户ID' + str(userID)+' 青豆(' +  str(score) + '),青豆少于3000，终止提现！\n'
             return
 
 
         print('用户ID',userID,' 青豆(',score,'),可以提现',money,'元')
+        allMessage =allMessage +'用户ID' + str(userID)+' 青豆(' +  str(score) + '),可以提现' +  str(money) + '元\n'
         userInfo=userInfo+'&money='+str(money)
         getMoneyZFB(userInfo) #默认提现到支付宝
         # getMoneyWX(userInfo)
@@ -69,6 +77,7 @@ def getScore(userInfo):
 
 ##支付宝提现        
 def getMoneyZFB(userInfo):
+    global allMessage
     url = 'https://user.youth.cn/FastApi/Alipay/withdraw.json'
     headers = {
     "User-Agent":'Mozilla/5.0 (Linux; Android 11; M2012K11AC Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36 hap/1.10/xiaomi com.miui.hybrid/1.10.3.3 com.youth.kandianquickapp/2.6.9 ({"packageName":"com.miui.quickappCenter","type":"url","extra":{"scene":""}})',
@@ -91,6 +100,7 @@ def getMoneyZFB(userInfo):
         #print(content)
         data=json.loads(content)
         print(data['message'])
+        allMessage =allMessage + str(data['message']+'\n')
     except Exception as e:
 
         print("请求错误：", e)
@@ -124,3 +134,4 @@ def getMoneyWX(userInfo):
 
 for userInfo in userInfos:
     getScore(userInfo.strip ())
+send("中青极速版提现", allMessage)
