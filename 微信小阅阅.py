e81a4c64be8de1d11ef3055d82b885363c5894dd
dunkuenfai <56312707+dunkuenfai@users.noreply.github.com>
2023-10-10 20:27:41 +0800

    Update 微信小阅阅.py

--- 微信小阅阅.py
+++ 微信小阅阅.py
@@ -23,7 +23,7 @@ ysmuidList = [
     "29a3867bb367ca26196405d8*bcea04d",
     "0d17ad03e98ecb56ea79f4b09e9a***",
     "e78214c19490440d7902d085**3e404*",
-    "40bda2e5ef6839a5f48f36ac7e6db5a3",
+    "40bda2e5ef6839a5f48f36a**e6db5a3",
     "1f07065ea0c7a7102e344ee1bd0*d187",
 ]
 
