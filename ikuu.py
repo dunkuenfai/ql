e13bdfe110cbd8d4be83b18693649f04611724c3
dunkuenfai <56312707+dunkuenfai@users.noreply.github.com>
2024-10-27 04:32:01 +0800

    Add files via upload

---
+++ ikuu.py
@@ -0,0 +1,41 @@
+"""
+cron: 1 0  2,22 * * *
+new Env('ikuu机场签到')
+
+"""
+
+import requests, json, re, os
+from sendNotify import send
+session = requests.session()
+# 配置用户名（一般是邮箱）
+email = os.environ.get('EMAIL')
+# 配置用户名对应的密码 和上面的email对应上
+passwd = os.environ.get('PASSWD')
+
+login_url = 'https://ikuuu.pw/auth/login'
+check_url = 'https://ikuuu.pw/user/checkin'
+info_url = 'https://ikuuu.pw/user/profile'
+
+header = {
+        'origin': 'https://ikuuu.pw',
+        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+}
+data = {
+        'email': email,
+        'passwd': passwd
+}
+try:
+    print('进行登录...')
+    response = json.loads(session.post(url=login_url,headers=header,data=data).text)
+    print(response['msg'])
+    # 获取账号名称
+    info_html = session.get(url=info_url,headers=header).text
+    # 进行签到
+    result = json.loads(session.post(url=check_url,headers=header).text)
+    print(result['msg'])
+    content = result['msg']
+    send("ikuu签到",content)
+    print('推送成功')
+except:
+    content = '签到失败'
+    send("ikuu签到",content)
