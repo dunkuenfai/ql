70987d749e90cd9f401c5bef87d490db0834f0dd
dunkuenfai <56312707+dunkuenfai@users.noreply.github.com>
2024-11-08 14:25:23 +0800

    Update ikuu.py

--- ikuu.py
+++ ikuu.py
@@ -19,7 +19,7 @@ check_url = base_url + '/user/checkin'
 info_url = base_url + '/user/profile'
 
 header = {
-        'origin': 'https://ikuuu.pw',
+        'origin': base_url,
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
 data = {
